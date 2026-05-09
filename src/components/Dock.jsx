4a64a9a5ec530db1776af4df77661c9ca6b19357
pip-po <officialomicumi@gmail.com>
2026-05-09 22:54:20 +0700

    feat: wrapper

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -73,7 +73,7 @@ const Dock = () => {
             <div ref={dockRef} className="dock-container">
                 {dockApps.map(({ id, name, icon, canOpen }) => (
                     <div key={id} className="relative flex justify-center" >
-                        <button type="button" className="dock-icon" aria-label={name} data-tooltip-id="dock-tooltip" onclick={() => toggleApp({ id, canOpen })} data-tooltip-content={name} data-tooltip-delay-show={150} disabled={!canOpen}>
+                        <button type="button" className="dock-icon" aria-label={name} data-tooltip-id="dock-tooltip" onClick={() => toggleApp({ id, canOpen })} data-tooltip-content={name} data-tooltip-delay-show={150} disabled={!canOpen}>
                             <img src={`/images/${icon}`} alt={name} loading="lazy" className={canOpen ? "" : "opacity-60"} />
                         </button>
                     </div>
