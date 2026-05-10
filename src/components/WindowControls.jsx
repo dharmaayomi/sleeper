12ad587d85107cba90e3fd88b8982901c00cbe96
pip-po <officialomicumi@gmail.com>
2026-05-10 09:54:22 +0700

    feat: location store

--- src/components/WindowControls.jsx
+++ src/components/WindowControls.jsx
@@ -1,15 +1,15 @@
-import useWindowStore from '#store/window'
-import React from 'react'
+import useWindowStore from "#store/Window";
+import React from "react";
 
 const WindowControls = ({ target }) => {
-  const { closeWindow, minimizeWindow } = useWindowStore()
+  const { closeWindow, minimizeWindow } = useWindowStore();
   return (
     <div id="window-controls">
       <div className="close" onClick={() => closeWindow(target)} />
-      <div className="minimize"/>
-      <div className="maximize"/>
+      <div className="minimize" />
+      <div className="maximize" />
     </div>
-  )
-}
+  );
+};
 
-export default WindowControls
+export default WindowControls;
