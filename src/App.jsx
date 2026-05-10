05702ab37b1820b5c181b542f54baf81009d0d51
pip-po <officialomicumi@gmail.com>
2026-05-10 10:36:41 +0700

    feat: contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import gsap from "gsap";
 import { Draggable } from "gsap/Draggable";
 import { Navbar, Welcome, Dock } from "#components";
-import { Terminal, Safari, Resume, Finder, Text, Image } from "#windows";
+import {
+  Terminal,
+  Safari,
+  Resume,
+  Finder,
+  Text,
+  Image,
+  Contact,
+} from "#windows";
 
 gsap.registerPlugin(Draggable);
 
@@ -17,6 +25,7 @@ const App = () => {
       <Finder />
       <Text />
       <Image />
+      <Contact />
     </main>
   );
 };
